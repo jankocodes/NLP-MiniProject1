4c39c88b6e719e21e75613e85ae4c53fd7438424
jankocodes <schuhmann.jannis@gmail.com>
2024-12-28 12:00:46 +0100

    training loop and dataset implementation added

---
+++ SkipgramDataset.py
@@ -0,0 +1,15 @@
+import torch
+from torch.utils.data import Dataset
+
+class SkipgramDataset(Dataset):
+    def __init__(self, pairs):
+        super().__init__()
+        self.pairs= pairs
+        
+    def __len__(self):
+        return len(self.pairs)
+    
+    def __getitem__(self, idx):
+        center, context= self.pairs[idx]
+        
+        return torch.tensor(center, dtype=torch.long), torch.tensor(context, dtype=torch.long)
