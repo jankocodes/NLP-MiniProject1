82f53dc09359b0216286ffb8ff7c2778125b98c5
jankocodes <schuhmann.jannis@gmail.com>
2025-01-09 17:04:59 +0100

    random search and first results

--- Skipgram.py
+++ Skipgram.py
@@ -8,27 +8,28 @@ class Skipgram(nn.Module):
         self.vocab_size= vocab_size
         self.embedding_dim= embedding_dim
         
-        self.embedding= nn.Embedding(vocab_size, embedding_dim)
+        self.center_embedding= nn.Embedding(vocab_size, embedding_dim)
         
-        self.output_embedding= nn.Embedding(vocab_size, embedding_dim)
+        self.context_embedding= nn.Embedding(vocab_size, embedding_dim)
         
         self.init_embeddings()
     
     def init_embeddings(self):
-        nn.init.xavier_uniform_(self.embedding.weight)
-        nn.init.xavier_uniform_(self.output_embedding.weight)
-    
+        
+        self.center_embedding.weight.data.uniform_(-1,1)
+        self.context_embedding.weight.data.uniform_(-1,1)
+
     def forward(self, center, context, negatives):
-        center_embedding= self.embedding(center) #(batch_size x embedding_dim)
+        center_emb= self.center_embedding(center) #(batch_size x embedding_dim)
         
-        context_embedding= self.output_embedding(context) #(batch_size x embedding_dim)
+        context_emb= self.context_embedding(context) #(batch_size x embedding_dim)
         
-        negatives_embedding= self.output_embedding(negatives) #(batch_size x num_neg_samples x embedding_dim)
+        negatives_emb= self.context_embedding(negatives) #(batch_size x num_neg_samples x embedding_dim)
         
-        positive_score= torch.sum(center_embedding*context_embedding, dim=1) #(batch_size)
+        positive_score= torch.sum(center_emb*context_emb, dim=1) #(batch_size)
 
         #(batch_size x num_neg_samples x embedding_dim) * (batch_size, embedding_dim, 1)
-        negative_score= torch.bmm(negatives_embedding, center_embedding.unsqueeze(2)).squeeze(2) #(batch_size, num_neg_samples)
+        negative_score= torch.bmm(negatives_emb, center_emb.unsqueeze(2)).squeeze(2) #(batch_size, num_neg_samples)
         
         return (positive_score, negative_score)
     
